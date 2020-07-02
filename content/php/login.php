01eea72e3040ce9565632e1dc6d4e93e63f88098
José Salazar <jose.mavo.125@gmail.com>
2020-07-02 15:22:40 -0430

    Asistente Fusión 6
    1.- Edición de login.php, ahora permite login de usuario por nombre de usuario como por correo sin presentar ningún problema. Antes el último commit permitía tanto usar usuario como correo pero cuando se usaba el usuario logueaba con cualquier password, esto ya no pasa

--- content/php/login.php
+++ content/php/login.php
@@ -9,11 +9,14 @@
 		$conectar = new conexion();
 		$conexion = $conectar->conectar();
 
-		$consulta = "SELECT * FROM usuarios WHERE nombre_usuario ='$usuario' OR correo_usuario = '$usuario' AND pass_usuario='$pass'";
+		$consulta = "SELECT * FROM usuarios WHERE nombre_usuario ='$usuario' AND pass_usuario='$pass'"; //Para acceso por nombre de usuario
+
+		$consulta2 = "SELECT * FROM usuarios WHERE correo_usuario = '$usuario' AND pass_usuario='$pass'"; //Para acceso por email
 
 		$resultado = $conexion->query($consulta);
+		$resultado2 = $conexion->query($consulta2);
 
-		if ($resultado->num_rows>0) {
+		if ($resultado->num_rows>0 || $resultado2->num_rows>0) { //validacion por usuario o correo + password
 			$_SESSION['login'] = TRUE;
 			// header('location: ../../index.php');
 			echo "<welcome>";
