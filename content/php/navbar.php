2217f38b8c267263fe78ef8eb3c1cb3c3b24218c
Eka <caiman.mistico@gmail.com>
2020-08-04 00:21:00 -0500

    0.0.3.6
    Se añaden imágenes para logo

--- content/php/navbar.php
+++ content/php/navbar.php
@@ -2,7 +2,7 @@
 
 <nav class="navbar mb-4 p-4 header-home-asistent " style="display: none;">
 	<a class="navbar-brand text-dark text-he font-weight-bold" href="?action=home">
-		<img src="content/img/isolated-3021541_640.png" width="30" height="30" class="d-inline-block align-top" alt="" loading="lazy">
+		<img src="content/img/logoAsistentev0.1.png" width="30" height="30" class="d-inline-block align-top" alt="" loading="lazy">
 		Asistente
 	</a>
 	<div id="menu_btn">
