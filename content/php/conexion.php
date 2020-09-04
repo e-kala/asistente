e21093afb26e548e2e5471b992d90bd64f0eb928
Eka <caiman.mistico@gmail.com>
2020-09-03 20:25:02 -0500

    0.0.6.3
    Actualización de los datos de conexión de la db

---
+++ content/php/conexion.php
@@ -0,0 +1,64 @@
+
+<?php
+	class conexion{
+		private $servidor="localhost";
+		private $usuario="u539976020_bonsai";
+		private $bd="u539976020_bonsai";
+		private $password=":i9i^Q|N>8w";
+
+		//private $usuario="u599230971_asistente";
+		//private $bd="u599230971_asistente";
+		//private $password="1q-2w-3e-4r-5t-";
+
+
+		public function __construct(){
+
+			$this->servidor;
+			$this->usuario;
+			$this->password;
+			$this->bd;
+		}
+
+		public function conectar(){
+			$conectar =
+			mysqli_connect(
+				$this->servidor,
+				$this->usuario,
+				$this->password,
+				$this->bd
+			);
+			return $conectar;
+		}
+
+	}
+
+	
+
+			
+	class mostrar{
+		public function mostrarDatos(){
+			$conectar = new conectar();
+			$conexion = $conectar->conexion();
+
+			$consulta = "SELECT * FROM ingresos, gastos";
+			$resultado = $conexion->query($consulta);
+
+			if ($resultado->num_rows > 0) {
+				$arreglo=array();
+				while ($fila = $result->fetch_assoc()) {
+					$arreglo[]=$fila;
+				}
+				$resultado->free();
+
+				$json = json_encode($arreglo);
+				echo $json;
+			}else {
+
+				echo 'no hay datos';
+			}
+		}
+	}
+
+
+
+?>
