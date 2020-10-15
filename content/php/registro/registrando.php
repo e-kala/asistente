41ebdd9e679aaf81ba349d17963bfbc178bf8eb1
JoeDyenz <joedyenz@gmail.com>
2020-10-15 18:11:19 -0500

    0.0.6.31
    Añadí validación de cuentas por correo único

--- content/php/registro/registrando.php
+++ content/php/registro/registrando.php
@@ -10,19 +10,26 @@
         $pass = $_POST['pass'];
         $array_cnfg = ["mode" => "light", "background" => "2/15.png", "transparency" => "1", "time_bal" => "5", "caducidad"=>"30", "aside_hidden" => "true", "pj_change"=>"./content/img/iconos/facehappy.gif", "pj_hidden"=>"true", "ingreso_minimo_mensual"=>"1000", "points" => "0"];
         $usr_cnfg_default = json_encode($array_cnfg);
-        $sql = "INSERT INTO usuarios(nombre_usuario, correo_usuario, pass_usuario, privilegios, usr_config)
-                VALUES('$usuario', '$mail', '$pass', 'free', '$usr_cnfg_default')";
-        $datos = utf8_encode($sql);
-        //$result = mysqli_query($conexion, $datos);
-        //echo $result;
+        
+        $sql = "SELECT correo_usuario FROM usuarios WHERE correo_usuario = '$mail";
+        $result = $conexion->query($sql);
+        if (($result === TRUE) && ($result->num_rows < 1))  {
+        
+	        $sql = "INSERT INTO usuarios(nombre_usuario, correo_usuario, pass_usuario, privilegios, usr_config)
+	                VALUES('$usuario', '$mail', '$pass', 'free', '$usr_cnfg_default')";
+	        //$datos = utf8_encode($sql);
+	        //$result = mysqli_query($conexion, $datos);
+	        //echo $result;
 
-        if ($conexion->query($sql) === TRUE) {
-            echo "New record created <successfully>";
-            // header('location: ../../../');
-        } else {
-            echo "Error: " . $sql . "<br>" . $conexion->error;
-        }
-
+	        if ($conexion->query($sql) === TRUE) {
+	            echo "New record created <successfully>";
+	            // header('location: ../../../');
+	        } else {
+	            echo "Error: " . $sql . "<br>" . $conexion->error;
+	        }   
+		}else{
+			echo "no hay datos que recibir";
+		}
     }else{
         echo "no hay datos que recibir";
     }
